12b23d09fc34cfeff7b1b460b2a5527bd0abddfd
juko <sakikaorucha027@gmail.com>
2017-11-06 11:25:51 +0900

    ホームページ11/6

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('.name').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).stop().addClass('mv07');
+		}
+		else{
+			$(this).stop().removeClass('mv07');
+		}
+	});
+});
